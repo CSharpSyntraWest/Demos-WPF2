53290f12f2950c243647a9b5533c172dbcfc1f5c
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-26 10:01:33 +0100

    Add project files.

---
+++ Demos-WPF2/AdviesGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demos_WPF2
+{
+    public class AdviesGenerator
+    {
+        private List<string> _antwoorden;
+        public AdviesGenerator()
+        {
+            _antwoorden = new List<string>();
+            VulAntwoorden();
+        }
+        private void VulAntwoorden()
+        {
+            _antwoorden.Add("42");
+            _antwoorden.Add("Het is bewolkt maar geen regen");
+            _antwoorden.Add("Met mij gaat het goed, dank u");
+        }
+        public string GetRandomAntwoord(string vraag)
+        {
+            int index = vraag.Length % 3;
+            return _antwoorden[index];
+        }
+    }
+}
---
+++ Demos-WPF2/TextBoxDemo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demos_WPF2
+{
+    /// <summary>
+    /// Interaction logic for TextBoxDemo.xaml
+    /// </summary>
+    public partial class TextBoxDemo : UserControl
+    {
+        public TextBoxDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGeefAntwoord_Click(object sender, RoutedEventArgs e)
+        {
+            AdviesGenerator adviesGenerator = new AdviesGenerator();
+            txtAntwoord.Text = adviesGenerator.GetRandomAntwoord(txtVraag.Text);
+        }
+    }
+}
