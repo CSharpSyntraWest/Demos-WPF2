59d896064ca9782a5ee34666ab7c89eeac52a190
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-26 14:00:46 +0100

    Demo DatePicker met ViewModel-binding

---
+++ Demos-navigatieControls/DatePickerDemo.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace Demos_navigatieControls
+{
+    /// <summary>
+    /// Interaction logic for DatePickerDemo.xaml
+    /// </summary>
+    public partial class DatePickerDemo : UserControl
+    {
+        public DatePickerDemo()
+        {
+            InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("nl-BE");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("nl-BE");
+            CultureInfo cd = CultureInfo.CreateSpecificCulture(CultureInfo.CurrentCulture.Name);
+            cd.DateTimeFormat.ShortDatePattern = "dd/MM/yyyy";
+            Thread.CurrentThread.CurrentCulture = cd;
+        }
+    }
+}
---
+++ Demos-navigatieControls/DatePickerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos_navigatieControls
+{
+    //MVVM - Model - ViewModel - View
+    public class DatePickerViewModel //ViewModel om te binden aan een View (DatePickerDemo view)
+    {
+        public DatePickerViewModel()
+        {
+            _datumVandaag = DateTime.Now.ToShortDateString();
+        }
+        private string _datumVandaag;
+        public string DatumVandaag
+        {
+            get { return _datumVandaag; }
+        }
+    }
+
+
+}
