e82e5c41744d9354212212b2b1e4e35e1ce1e19b
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-26 10:48:47 +0100

    WPF .NET Framework demos

---
+++ Demos-WPF2-Framework/TextBoxDemo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demos_WPF2_Framework
+{
+    /// <summary>
+    /// Interaction logic for TextBoxDemo.xaml
+    /// </summary>
+    public partial class TextBoxDemo : UserControl
+    {
+        public TextBoxDemo()
+        {
+            InitializeComponent();
+        }
+        private void btnGeefAntwoord_Click(object sender, RoutedEventArgs e)
+        {
+            AdviesGenerator adviesGenerator = new AdviesGenerator();
+            txtAntwoord.Text = adviesGenerator.GetRandomAntwoord(txtVraag.Text);
+        }
+    }
+}
