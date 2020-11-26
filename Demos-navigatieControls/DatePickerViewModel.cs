ff581b3cc2af078c875894495791ca51498dd039
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-26 15:38:24 +0100

    Demo Binding Controls

--- Demos-navigatieControls/DatePickerViewModel.cs
+++ Demos-navigatieControls/DatePickerViewModel.cs
@@ -7,10 +7,21 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 /*
- * Oefening WPF Binding
+ * Oefening 1 WPF Binding
  * Maak een nieuwe public property GeboorteDatum met enkel get die je geboortedatum bindt aan de Eerste datepicker control
  * initialiseer de geboortedatum in de constructor van DatePickerViewModel
  * Ga daarna naar DatePickerDemo.xaml en zet de Text="{Binding GeboorteDatum}" van de bovenste datepicker control
+ * 
+ * Oefening 2 wPF Binding
+ * Maak een nieuwe public Property Naam en private field _naam in DatePickerViewModel
+ * initialiseer _naam in de contstructor met je eigen naam
+ * Maak voor Property Naam een get{...} en set{...} en zet de volgende 2 lijnen erbij in de set:
+ *              OnPropertyChanged();
+                Debug.WriteLine(_naam);
+* Ga naar  DatePickerDemo.xaml en zet een TwoWay binding op de property Text:
+* <TextBox Text="{Binding Naam,UpdateSourceTrigger=PropertyChanged,Mode=TwoWay}"/>
+ 
+ 
  */
 namespace Demos_navigatieControls
 {
